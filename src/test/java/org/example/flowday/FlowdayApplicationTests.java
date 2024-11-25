d40326a5bcc760f568ec322a0efc3ec90b1dad4e
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-25 20:37:07 +0900

    Mod: 테스트 환경 설정

--- src/test/java/org/example/flowday/FlowdayApplicationTests.java
+++ src/test/java/org/example/flowday/FlowdayApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.flowday;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:/application-test.yml")
 class FlowdayApplicationTests {
 
     @Test
