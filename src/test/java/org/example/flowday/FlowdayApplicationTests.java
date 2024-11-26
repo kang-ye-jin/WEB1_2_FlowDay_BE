4c1a26d9b338401fe1ffe0a25e181c239b222063
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-26 10:52:35 +0900

    Mod

--- src/test/java/org/example/flowday/FlowdayApplicationTests.java
+++ src/test/java/org/example/flowday/FlowdayApplicationTests.java
@@ -2,10 +2,11 @@ package org.example.flowday;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:/application-test.yml")
+@ActiveProfiles("test")
 class FlowdayApplicationTests {
 
     @Test
