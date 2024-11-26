fbb65411431a661032283ba65ce85f762ff488a0
kang-ye-jin <yejin331@gachon.ac.kr>
2024-11-26 10:44:07 +0900

    Mod

--- src/test/java/org/example/flowday/FlowdayApplicationTests.java
+++ src/test/java/org/example/flowday/FlowdayApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-//@SpringBootTest
-//@TestPropertySource(locations = "classpath:/application-test.yml")
+@SpringBootTest
+@TestPropertySource(locations = "classpath:/application-test.yml")
 class FlowdayApplicationTests {
 
     @Test
